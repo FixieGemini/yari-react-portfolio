49d4ee3d2ab20f8ff4e86400111dc7c0a3894012
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-24 11:36:57 -0700

    adding footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<a href="https://github.com/FixieGemini" target="_blank"><img className="footerContact" src={require("../../assets/icons/github.png")} alt="Github"/></a>
+			<a href="https://www.linkedin.com/in/yariel-crespo-6622b4249/" target="_blank"><img className="footerContact" src={require("../../assets/icons/linkedin.png")} alt="LinkedIn"/></a>
+			<a href="tel:5203329541" target="_blank"><img className="footerContact" src={require("../../assets/icons/phone.jpg")} alt="Phone"/></a>
+			<a href="mailto:yariel.crespo@gmail.com" target="_blank"><img className="footerContact" src={require("../../assets/icons/email.jpg")} alt="Email"/></a>
+		</footer>
+	);
+}
+
+export default Footer;
