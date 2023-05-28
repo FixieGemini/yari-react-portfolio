4706b26758019b69ca714751ffe36fc91c87c885
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-28 08:35:42 -0700

    footer complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "./FooterStyles.css";
 
 import React from 'react'
 
-import { FaHome } from "react-icons/fa";
+import { FaHome, FaPhone, FaMailBulk, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -10,11 +10,35 @@ const Footer = () => {
         <div className="footer-container">
             <div className="left">
                 <div className="location">
-                    <FaHome />
+                    <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+                <div>
+                  <p>10290 N. Renard Pl.</p>
+                  <p>Tucson, AZ.</p>
                 </div>
             </div>
+            <div className="phone">
+              <h4>
+              <FaPhone size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+              520 332-9541
+              </h4>
+            </div>
+            <div className="email">
+              <h4>
+              <FaMailBulk size={20} style={{ color: "#fff", marginRight: "2rem" }} />
+              yariel.crespo@gmail.com
+              </h4>
+            </div>
+          </div>
 
-            <div className="right"></div>
+            <div className="right">
+              <h4>About Site</h4>
+              <p>This portfolio was made using html, css, node.js and react.js with the help of Fahimul Kabir and their Tech2etc tutorial. Thank you for checking it out!</p>
+              <div className="social">
+              <FaGithub size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+              <FaLinkedin size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+              <FaInstagram size={30} style={{ color: "#fff", marginRight: "2rem" }} />
+              </div>
+            </div>
         </div>
     </div>
   )
