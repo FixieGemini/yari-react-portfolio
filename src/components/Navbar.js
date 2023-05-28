49e43812acf02bd0a24853bea224174f92cdefe2
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-27 18:27:12 -0700

    navbar and hero coming along nicely

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import "./NavbarStyles.css";
 
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "react-router-dom";
 
+import { FaBars, FaTimes } from "react-icons/fa";
+
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);  
+
   return (
     <div className="header">
         <Link to="/yari-react-portfolio">
             <h1>Portfolio</h1>
         </Link>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to ="/yari-react-portfolio">Home</Link>
             </li>
@@ -23,9 +28,16 @@ const Navbar = () => {
                 <Link to ="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (
+            <FaTimes size={20} style={{ color: "#fff" }} />
+            ) : (
+            <FaBars size={20} style={{ color: "#fff" }} /> 
+            )}
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
 
