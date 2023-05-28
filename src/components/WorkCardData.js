99a013867778bce0963644fe33760ecbc4735391
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-28 14:45:30 -0700

    project cards

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,29 @@
+import pro1 from "../assets/weathercats.png";
+import pro2 from "../assets/sharemyproject.png";
+import pro3 from "../assets/appointme.png";
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Weather Cats",
+        text: "Cats cheer you up!",
+        view: "https://github.com/FixieGemini",
+        source: "https://github.com/FixieGemini?tab=repositories"
+    },
+    {
+        imgsrc: pro2,
+        title: "Share My Project",
+        text: "Show your work to the class!",
+        view: "https://github.com/FixieGemini",
+        source: "https://github.com/FixieGemini?tab=repositories"
+    },
+    {
+        imgsrc: pro3,
+        title: "AppointMe",
+        text: "Get scheduled!",
+        view: "https://github.com/FixieGemini",
+        source: "https://github.com/FixieGemini?tab=repositories"
+    }
+];
+
+export default ProjectCardData;
