bf89a9a77b3ed419cff98f8991d00107f3be7521
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-28 09:55:05 -0700

    heroimg2 and some props

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div>
+      <Navbar />
+      <HeroImg2 heading="CONTACT ME" text="Lets connect!"/>
+      <Footer />
+    </div>
+  );
+};
 
 export default Contact
