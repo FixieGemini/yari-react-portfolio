56a6ff5ed7ea5cd4dfba0ba44c8fcb34492da539
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-29 11:48:53 -0700

    about content and styling

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,3 +1,32 @@
+import { Link } from "react-router-dom";
+import "./AboutContentStyles.css";
+import Avatar from "../assets/avatar.jpeg";
+
+import React from 'react';
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <div className="img-container">
+                <img src={Avatar} className="img" alt="Yariel Crespo" />
+            </div>
+        </div>
+
+        <div className="right">
+            <h1>Who am I?</h1>
+            <p>I am an aspiring full stack web developer. I love solving complex problems and creating amazing apps!</p>
+            <Link to="/contact">
+                <button className="btn">contact</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent
+
+
 // import React from "react";
 
 // export default function About() {
--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -6,9 +6,9 @@ import { NavLink } from "react-router-dom";
 
 const WorkCard = (props) => {
   return (
-    <div className="work-container">
-        <h1 className="project-heading"></h1>
-        <div className="project-container">
+    // <div className="work-container">
+    //     <h1 className="project-heading"></h1>
+    //     <div className="project-container">
             <div className="project-card">
                 <img src={props.imgsrc} alt="my weathercats group project screen capture" />
                 <h2 className="project-title">{props.title}</h2>
@@ -20,8 +20,8 @@ const WorkCard = (props) => {
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+    //     </div>
+    // </div>
   )
 }
 
