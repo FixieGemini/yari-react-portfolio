cf32ffbf5d2b197a31a587260209dcb734827c01
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-29 12:15:01 -0700

    contact form and style added

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import Form from "../components/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="CONTACT ME" text="Lets connect!"/>
+      <Form />
       <Footer />
     </div>
   );
