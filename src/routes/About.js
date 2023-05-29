f695123addd2d6cf52fb87dd234506afa71f68f9
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-29 12:27:48 -0700

    smart funny cool nice

--- src/routes/About.js
+++ src/routes/About.js
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <div>
       <Navbar />
-      <HeroImg2 heading="ABOUT" text="I am a full stack web developer"/>
+      <HeroImg2 heading="ABOUT ME" text="Smart, funny, cool, nice."/>
       <AboutContent />
       <Footer />
     </div>
