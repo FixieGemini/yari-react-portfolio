eee69ac18846266b52520763cf2aef7276b38e84
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-05-30 21:15:16 -0700

    added bio and project descriptions

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -15,7 +15,17 @@ const AboutContent = () => {
 
         <div className="right">
             <h1>Who am I?</h1>
-            <p>I am an aspiring full stack web developer. I love solving complex problems and creating amazing apps!</p>
+            <p>Hello there! I'm Yariel Crespo, but you can call me Yari. I'm a passionate and dedicated individual who's embarking on an exciting career transition from retail to web development. With nearly 20 years of experience in customer service and sales, I've honed my skills in providing exceptional service and building strong relationships with clients.</p>
+
+            <p>Born and raised in Tucson, Arizona, I have a deep appreciation for the beauty of the Sonoran desert. However, my true passion lies in the world of web development. I aspire to become a proficient full stack web developer, collaborating with others to solve complex problems and create captivating web applications.</p>
+
+            <p>Recently, I completed a rigorous coding and web development boot camp through the University of Arizona, earning a B+ grade. This intensive program allowed me to gain practical experience in a wide range of technologies. I've worked extensively with HTML, CSS, bootstrap, JavaScript, Git, GitHub, Heroku, Node.js, JSON, mySQL, MongoDB, express.js, and React.js.</p>
+
+            <p>During the boot camp, I also acquired valuable knowledge in areas such as search engine optimization, object-oriented programming, responsive design, agile methodologies, coding best practices, and computer science fundamentals. With a thirst for knowledge, I'm eager to continue learning and expanding my skills in order to stay ahead in this rapidly evolving industry.</p>
+
+            <p>I invite you to explore my portfolio, which showcases my work and the projects I've completed. If you're interested in collaborating or have any inquiries, feel free to reach out using the contact information provided.</p>
+
+            <p>Let's connect and bring your web development ideas to life!</p>
             <Link to="/contact">
                 <button className="btn">contact</button>
             </Link>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -6,21 +6,21 @@ const ProjectCardData = [
     {
         imgsrc: pro1,
         title: "Weather Cats",
-        text: "Cats cheer you up!",
+        text: "A responsive web application that serves photos of cats, basic weather information, and inspirational quotes to users.",
         view: "https://github.com/FixieGemini",
         source: "https://github.com/FixieGemini?tab=repositories"
     },
     {
         imgsrc: pro2,
         title: "Share My Project",
-        text: "Show your work to the class!",
+        text: "A website for my UofA web development boot camp cohort to share their group projects",
         view: "https://github.com/FixieGemini",
         source: "https://github.com/FixieGemini?tab=repositories"
     },
     {
         imgsrc: pro3,
         title: "AppointMe",
-        text: "Get scheduled!",
+        text: "AppointMe! is an easy to use appointment scheduler for busy professionals, freelancers, and anyone that just wants to better utilize their time. With its simple interface design users can schedule and manage appointments with a few clicks.",
         view: "https://github.com/FixieGemini",
         source: "https://github.com/FixieGemini?tab=repositories"
     }
